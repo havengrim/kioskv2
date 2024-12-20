d93752db9eebefd194c83b84952f52771416eb33
havengrim <angeloguiao.personal@gmail.com>
2024-12-20 10:42:59 +0300

    Zzzz

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,23 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
-theme: {
-  extend: {
-    borderRadius: {
-      lg: 'var(--radius)',
-      md: 'calc(var(--radius) - 2px)',
-      sm: 'calc(var(--radius) - 4px)'
-    },
-    colors: {}
-  }
-},
-plugins: [require("tailwindcss-animate")],
-}
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      },
+      colors: {
+        primary: {
+          light: '#F3E8FF',
+          DEFAULT: '#A855F7',
+          dark: '#7E22CE'
+        },
+        secondary: '#64748B'
+      }
+    }
+  },
+  plugins: [require("tailwindcss-animate")]
+};
