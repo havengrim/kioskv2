03901375d0daa278faa39f149ba9ed89c809595c
havengrim <angeloguiao.personal@gmail.com>
2024-12-20 10:31:45 +0300

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,17 @@
-// tailwind.config.js (CommonJS syntax)
-module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
 
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   darkMode: ["class"],
-//   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
-// theme: {
-//   extend: {
-//     borderRadius: {
-//       lg: 'var(--radius)',
-//       md: 'calc(var(--radius) - 2px)',
-//       sm: 'calc(var(--radius) - 4px)'
-//     },
-//     colors: {}
-//   }
-// },
-// plugins: [require("tailwindcss-animate")],
-// }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+theme: {
+  extend: {
+    borderRadius: {
+      lg: 'var(--radius)',
+      md: 'calc(var(--radius) - 2px)',
+      sm: 'calc(var(--radius) - 4px)'
+    },
+    colors: {}
+  }
+},
+plugins: [require("tailwindcss-animate")],
+}
