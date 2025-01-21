12edc9e2add962a413b40a9406f4470c049dc721
havengrim <angeloguiao.personal@gmail.com>
2025-01-21 22:31:03 +0800

    late

--- src/Home.tsx
+++ src/Home.tsx
@@ -16,8 +16,18 @@ import { Dialog, DialogTrigger, DialogContent, DialogTitle, DialogDescription, D
 import Typing from 'react-typing-effect';
 import AOS from "aos";
 import "aos/dist/aos.css"; // import AOS styles
+import Lottie from 'react-lottie';
+import animationData from '@/assets/lottie.json';
 
 const Home = () => {
+  const defaultOptions = {
+    loop: true,  // Set to true if you want the animation to loop
+    autoplay: true,  // Set to true to autoplay the animation
+    animationData,  // The imported Lottie animation data
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice', // Keeps the aspect ratio of the animation
+    },
+  };
   const [selectedQRCode, setSelectedQRCode] = useState<string | null>(null);
   useEffect(() => {
     AOS.init({
@@ -73,13 +83,13 @@ const Home = () => {
 
         {/* Hero Section */}
       <div className="container flex flex-col px-6 py-4 mx-auto space-y-6 lg:h-[32rem] lg:py-16 lg:flex-row lg:items-center">
-  <div className="flex flex-col items-center w-full lg:flex-row lg:w-1/2">
-    <div className="flex justify-center order-2 mt-6 lg:mt-0 lg:space-y-3 lg:flex-col">
-      <button className="w-3 h-3 mx-2 bg-blue-500 rounded-full lg:mx-0 focus:outline-none"></button>
-      <button className="w-3 h-3 mx-2 bg-gray-300 rounded-full lg:mx-0 focus:outline-none hover:bg-blue-500"></button>
-      <button className="w-3 h-3 mx-2 bg-gray-300 rounded-full lg:mx-0 focus:outline-none hover:bg-blue-500"></button>
-      <button className="w-3 h-3 mx-2 bg-gray-300 rounded-full lg:mx-0 focus:outline-none hover:bg-blue-500"></button>
-    </div>
+        <div className="flex flex-col items-center w-full lg:flex-row lg:w-1/2">
+          <div className="flex justify-center order-2 mt-6 lg:mt-0 lg:space-y-3 lg:flex-col">
+            <button className="w-3 h-3 mx-2 bg-blue-500 rounded-full lg:mx-0 focus:outline-none"></button>
+            <button className="w-3 h-3 mx-2 bg-gray-300 rounded-full lg:mx-0 focus:outline-none hover:bg-blue-500"></button>
+            <button className="w-3 h-3 mx-2 bg-gray-300 rounded-full lg:mx-0 focus:outline-none hover:bg-blue-500"></button>
+            <button className="w-3 h-3 mx-2 bg-gray-300 rounded-full lg:mx-0 focus:outline-none hover:bg-blue-500"></button>
+          </div>
 
         <div className="max-w-lg lg:mx-12 lg:order-2">
         <h1 className="text-3xl font-semibold tracking-wide text-gray-800 dark:text-white lg:text-4xl">
@@ -97,7 +107,7 @@ const Home = () => {
       </div>
 
       <div className="flex items-center justify-center w-full h-96 lg:w-1/2">
-        <img className="object-cover w-full h-full max-w-2xl rounded-md" src="https://images.unsplash.com/photo-1579586337278-3befd40fd17a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" alt="apple watch photo" />
+      <Lottie options={defaultOptions} height={400} width={400} />
       </div>
     </div>
 
